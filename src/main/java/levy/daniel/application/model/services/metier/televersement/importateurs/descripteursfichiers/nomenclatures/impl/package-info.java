f8e48f1ca1e3bfda9e5590e2a8240bca23c95ce3
dan <dan@10.77.222.27>
2019-05-14 14:16:26 +0200

    COMMIT_&_PUSH_DL_DOMICILE

--- src/main/java/levy/daniel/application/model/services/metier/televersement/importateurs/descripteursfichiers/nomenclatures/impl/package-info.java
+++ src/main/java/levy/daniel/application/model/services/metier/televersement/importateurs/descripteursfichiers/nomenclatures/impl/package-info.java
@@ -3,7 +3,19 @@
  * Ce package contient tous les ImporteursNomenclature concrets.<br/>
  * <br/>
  *
- * - Exemple d'utilisation :<br/>
+ * <p>
+ * - Exemple d'utilisation :
+ * </p>
+ * <p>
+ * <code> // Instanciation d'un ImporteurLexique</code><br/>
+ * <code><b>IImporteurLexique IMPORTEUR_LEXIQUE = new ImporteurLexique();</b></code><br/>
+ * <code> // Import du fichier de lexique lexiqueFile</code><br/>
+ * <code><b>Map&lt;String, String&gt; resultat = IMPORTEUR_LEXIQUE.importerLexiqueEnUtf8(lexiqueFile);</b></code><br/>
+ * <code> // Récupération du Set de clés possibles</code><br/>
+ * <code><b>Set&lt;String&gt; clesPossiblesSet = IMPORTEUR_LEXIQUE.getClesPossiblesSet();</b></code><br/>
+ * <code> // Génère le lexique importé dans le fichier nomenclatureGenereeFile avec en-tête et encodée en UTF-8.</code><br/>
+ * <code><b>IMPORTEUR_LEXIQUE.genererNomenclatureCsvFile(true, nomenclatureGenereeFile, charsetUtf8);</b></code><br/>
+ * </p>
  *<br/>
  * 
  * - Mots-clé :<br/>
