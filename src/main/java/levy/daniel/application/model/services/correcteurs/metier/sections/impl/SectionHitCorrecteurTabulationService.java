948d508a15e75b088597fac1f54669fa38aec643
dan <dan@10.77.222.242>
2019-09-17 09:46:42 +0200

    COMMIT_&_PUSH_DL_DOMICILE

--- src/main/java/levy/daniel/application/model/services/correcteurs/metier/sections/impl/SectionHitCorrecteurTabulationService.java
+++ src/main/java/levy/daniel/application/model/services/correcteurs/metier/sections/impl/SectionHitCorrecteurTabulationService.java
@@ -11,6 +11,7 @@ import java.util.SortedMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
@@ -87,6 +88,17 @@ public class SectionHitCorrecteurTabulationService {
 	 */
 	public static final Path PATH_ABSOLU_REPERTOIRE_TEMP 
 		= PATH_ABSOLU_PRESENT_PROJET.resolve("temp");
+	
+	/**
+	 * "\\t".
+	 */
+	public static final  String MOTIF_TABULATION = "\\t";
+		
+	/**
+	 * Pattern.compile(MOTIF_TABULATION).
+	 */
+	public static final Pattern PATTERN_TABULATION 
+		= Pattern.compile(MOTIF_TABULATION);
 
 	/**
 	 * LOG : Log : 
@@ -166,10 +178,12 @@ public class SectionHitCorrecteurTabulationService {
 			while (matcherTabulation.find()) {
 				
 				final String tabulationString = matcherTabulation.group();
-				final int positionDebutTab = matcherTabulation.start();
-				final int positionFinTab = matcherTabulation.end();
+				final int positionDebutTab = matcherTabulation.start() + 1;
+				final int positionFinTab = matcherTabulation.end() + 1;
 					
 				String nomChamp = "";
+				int debutChamp = 0;
+				int finChamp = 0;
 				
 				final SortedMap<Integer, IDescriptionChamp> descriptionMap 
 					= FactoryDescription.getDecriptionHitMap();
@@ -180,16 +194,17 @@ public class SectionHitCorrecteurTabulationService {
 						= (DescriptionChampHit) desc;
 					
 					final String champLocal = descHit.getChampJava();
-					final int debutChamp = descHit.getColonneDebut();
-					final int finChamp = descHit.getColonneFin();
+					debutChamp = descHit.getColonneDebut();
+					finChamp = descHit.getColonneFin();
 					
 					if (positionDebutTab >= debutChamp && positionFinTab <= finChamp) {
 						nomChamp = champLocal;
+						
 						break;
 					}
 				}
 				
-				System.out.println("ligne " + compteur + " - position début tabulation : " + positionDebutTab + " - position fin tabulation : " + positionFinTab + "nom du champ : " + nomChamp);
+				System.out.println("ligne " + compteur + " - position début tabulation : " + positionDebutTab + " - position fin tabulation : " + positionFinTab + " - nom du champ : " + nomChamp + " - début du champ : " + debutChamp + " - fin du champ : " + finChamp);
 				
 			}
 			
@@ -197,8 +212,250 @@ public class SectionHitCorrecteurTabulationService {
 		
 	}
 	
+
+	
+	/**
+	 * .<br/>
+	 * : void :  .<br/>
+	 * @throws Exception 
+	 */
+	public static void supprimerTabulations() throws Exception {
+				
+		final Path fichierDonneesPath 
+			= PATH_ABSOLU_TEST_JEUX_ESSAI.resolve("2018/HITDIRA2018.txt");
+		final File fichierDonnees = fichierDonneesPath.toFile();
+		final Charset charsetAnsi = Charset.forName("Windows-1252");
+
+		final List<String> listeLignes 
+			= Files.readAllLines(fichierDonneesPath, charsetAnsi);
+		
+		
+		int compteur = 0;
+		
+		for (final String ligne : listeLignes) {
+			
+			compteur++;
+			
+			System.out.println("******ligne ORIGINALE : " + ligne);
+			
+			System.out.println(String.format(
+					LocaleManager.getLocaleParDefautApplication()
+					, "******ligne %1$-7d - longueur : %2$-3d", compteur, ligne.length()));
+						
+			final String ligneEpureeDesTabs = supprimerTabulation(compteur, ligne);
+			
+			System.out.println(ligneEpureeDesTabs);
+			System.out.println("longueur de la ligne épurée : " + ligneEpureeDesTabs.length());
+			
+		}
+		
+	}
+	
+
+
+	/**
+	 * .<br/>
+	 * <br/>
+	 * @param pCompteur 
+	 *
+	 * @param pLigne
+	 * @return : String :  .<br/>
+	 * @throws Exception 
+	 */
+	public static String supprimerTabulation(final int pCompteur, final String pLigne) throws Exception {
+		
+		final Matcher matcherTabulation = PATTERN_TABULATION.matcher(pLigne);
+		
+		System.out.println();
+		System.out.println("pLigne : " + pLigne);
+		System.out.println("LONGUEUR de pLigne : " + pLigne.length());
+		
+		if (matcherTabulation.find()) {
+			
+			final String tabulationString = matcherTabulation.group();
+			final int positionDebutTab = matcherTabulation.start() + 1;
+			final int positionFinTab = matcherTabulation.end() + 1;
+				
+			String nomChamp = "";
+			int debutChamp = 0;
+			int finChamp = 0;
+			int longueurChamp = 0;
+			
+			int debutChampSuivant = 0;
+			
+			final DescriptionChampHit descHit 
+				= fournirDescChamp(positionDebutTab, positionFinTab);
+			
+			if (descHit == null) {
+				return null;
+			}
+			
+			final DescriptionChampHit descHitSuivant 
+				= fournirDescChampSuivant(descHit);
+			
+			if (descHitSuivant == null) {
+				return null;
+			}
+
+			nomChamp = descHit.getNomChampJava();
+			debutChamp = descHit.getColonneDebut();
+			finChamp = descHit.getColonneFin();
+			longueurChamp = descHit.getLongueur();
+			
+			debutChampSuivant = descHitSuivant.getColonneDebut();
+						
+			String ligne = pLigne;
+
+			System.out.println("ligne " + pCompteur + " - position début tabulation : " + positionDebutTab + " - position fin tabulation : " + positionFinTab + " - nom du champ : " + nomChamp + " - début du champ : " + debutChamp + " - fin du champ : " + finChamp + " - longueur du champ : " + longueurChamp);
+			System.out.println("longueur de ligne : " + ligne.length());
+			
+			final String debutLigne = StringUtils.substring(ligne, 0, debutChamp - 1);
+			
+			System.out.println();
+			System.out.println("DEBUT ligne : " + debutLigne);
+			System.out.println("longueur de DEBUT ligne : " + debutLigne.length());
+			
+			final String espacesDeRemplacement = fournirEspaces(longueurChamp);
+			System.out.println();
+			System.out.println("longueur espaces de remplacement : " + espacesDeRemplacement.length());
+			
+			final String debutLigneAugmente = debutLigne + espacesDeRemplacement;
+			
+			System.out.println();
+			System.out.println("DEBUT ligne AUGMENTE : " + debutLigneAugmente);
+			System.out.println("longueur de DEBUT ligne AUGMENTE : " + debutLigneAugmente.length());
+			
+			ligne = StringUtils.replaceOnce(ligne, debutLigne, debutLigneAugmente);
+			
+//			final String finLigne = StringUtils.substring(ligne, debutChampSuivant - 1, ligne.length());
+//			
+//			System.out.println();
+//			System.out.println("FIN ligne : " + finLigne);
+//			System.out.println("longueur de FIN ligne : " + finLigne.length());
+//			
+//			ligne = debutLigne + espacesDeRemplacement + finLigne;
+			
+			System.out.println();
+			System.out.println("ligne reconstituee : " + ligne);
+			System.out.println("longueur de ligne reconstituee : " + ligne.length());
+			
+			matcherTabulation.reset();
+			
+			return supprimerTabulation(pCompteur, ligne);
+
+		}
+			
+		return pLigne;
+
+	}
+	
+
+	
+	/**
+	 * retourne la DescriptionChampHit du champ HIT qui contient 
+	 * complètement le segment [pDebut - pFin] (1-based) 
+	 * dans un fichier HIT.<br/>
+	 * <br/>
+	 * - retourne null si aucun champ du HIT n'englobe entièrement 
+	 * [pDebut - pFin].<br/>
+	 * <br/>
+	 *
+	 * @param pDebut : int : début de la position 1-based du segment.
+	 * @param pFin : int : fin de la position 1-based du segment.
+	 * 
+	 * @return : DescriptionChampHit.<br/>
+	 * 
+	 * @throws Exception 
+	 */
+	public static DescriptionChampHit fournirDescChamp(
+			final int pDebut, final int pFin) throws Exception {
+		
+		final SortedMap<Integer, IDescriptionChamp> descriptionMap 
+		= FactoryDescription.getDecriptionHitMap();
+		
+		for (final IDescriptionChamp desc : descriptionMap.values()) {
+			
+			final DescriptionChampHit descHit 
+				= (DescriptionChampHit) desc;
+			
+			final int debutChamp = descHit.getColonneDebut();
+			final int finChamp = descHit.getColonneFin();
+			
+			if (pDebut >= debutChamp && pFin <= finChamp) {
+				return descHit;
+			} 
+		}
+		
+		return null;
+		
+	} // Fin de fournirDescChamp(...)._____________________________________
+	
+
+	
+	/**
+	 * retourne le successeur immédiat de pDesc 
+	 * dans la description de fichier.<br/>
+	 * <br/>
+	 * - retourne null si pDesc == null.<br/>
+	 * - retourne null si pDesc n'a pas de successeur 
+	 * dans la description de fichier.<br/>
+	 * <br/>
+	 *
+	 * @param pDesc : DescriptionChampHit.<br/>
+	 * 
+	 * @return : DescriptionChampHit.<br/>
+	 * 
+	 * @throws Exception
+	 */
+	public static DescriptionChampHit fournirDescChampSuivant(
+					final DescriptionChampHit pDesc) throws Exception {
+		
+		/* retourne null si pDesc == null. */
+		if (pDesc == null) {
+			return null;
+		}
+				
+		final SortedMap<Integer, IDescriptionChamp> descriptionMap 
+			= FactoryDescription.getDecriptionHitMap();
+		
+		final Integer numeroOrdre = pDesc.getOrdreChamps();
+		final Integer numeroOrdreAChercher = numeroOrdre + 1;
+		
+		/* retourne null si pDesc n'a pas de successeur 
+		 * dans la description de fichier. */
+		if (numeroOrdreAChercher > descriptionMap.size()) {
+			return null;
+		}
+		
+		return (DescriptionChampHit) descriptionMap.get(numeroOrdreAChercher);
+		
+	} // Fin de fournirDescChampSuivant(...).______________________________
+
 	
 	
+	/**
+	 * .<br/>
+	 * <br/>
+	 *
+	 * @param pLongueurChamp
+	 * @return : String :  .<br/>
+	 */
+	public static String fournirEspaces(final int pLongueurChamp) {
+		
+		if (pLongueurChamp == 0) {
+			return "";
+		}
+		
+		String resultat = "";
+		
+		for (int i = 0; i < pLongueurChamp; i++) {
+			resultat = resultat + " ";
+		}
+		
+		return resultat;
+		
+	}
+
 	
 	
 	/**
@@ -210,8 +467,9 @@ public class SectionHitCorrecteurTabulationService {
 	 */
 	public static void main(final String... pArgs) throws Exception {
 		
-		detecterTab();
-		
+//		supprimerTabulations();
+		final String ligne62 ="330504601310017 02300 411 601ECH.25              0400703A 10                0440507S47 EXTER ET INTER  0440013003800003800          000       0290      123374 1407    114879	119300	 123949	  126142   125648   126199   135383   135208   127209	125708	 109008	  111569               171232491 1407    161237781 1367    151204371 1377    140000001         130000001         113791   119552   120745   125771   123307   125866   133235   131262   125333   123944   119420   116762                                      ";
+		supprimerTabulation(62, ligne62);
 	} // Fin de main(...)._________________________________________________
 
 	
