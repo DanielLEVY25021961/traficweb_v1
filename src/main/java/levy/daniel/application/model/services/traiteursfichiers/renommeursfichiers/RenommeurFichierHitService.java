c4aaaf14c08f40534f9c5e507937681bd47d241d
dan <dan@10.77.222.242>
2019-09-22 22:13:57 +0200

    COMMIT_&_PUSH_DL_DOMICILE

---
+++ src/main/java/levy/daniel/application/model/services/traiteursfichiers/renommeursfichiers/RenommeurFichierHitService.java
@@ -0,0 +1,46 @@
+package levy.daniel.application.model.services.traiteursfichiers.renommeursfichiers;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import levy.daniel.application.model.metier.televersement.ITeleversement;
+
+/**
+ * CLASSE RenommeurFichierHitService :<br/>
+ * .<br/>
+ * <br/>
+ *
+ * - Exemple d'utilisation :<br/>
+ *<br/>
+ * 
+ * - Mots-clé :<br/>
+ * <br/>
+ *
+ * - Dépendances :<br/>
+ * <br/>
+ *
+ *
+ * @author dan Lévy
+ * @version 1.0
+ * @since 22 sept. 2019
+ *
+ */
+public class RenommeurFichierHitService {
+
+	// ************************ATTRIBUTS************************************/
+
+	/**
+	 * LOG : Log : 
+	 * Logger pour Log4j (utilisant commons-logging).
+	 */
+	@SuppressWarnings("unused")
+	private static final Log LOG 
+		= LogFactory.getLog(RenommeurFichierHitService.class);
+
+	// *************************METHODES************************************/
+	
+	File traiterFichier(ITeleversement pTeleversement) {
+		
+	}
+	
+} // FIN DE LA CLASSE RenommeurFichierHitService.----------------------------
